1781943b29adcd290355f1ae611a1b65edc60c4c
Jubin Joy <jubinjoy432@gmail.com>
2025-07-23 15:17:55 +0530

    Added Program 5

---
+++ Program5.c
@@ -0,0 +1,50 @@
+/*To sort the elements in an array in ascending order
+@Jubin Joy 27/03/2025*/
+#include<stdio.h>
+void read(int a[],int n)
+{
+	int i;
+	printf("Enter the elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+}
+void sort(int a[],int n)
+{
+	int i,j,temp;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+}
+void disp(int a[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
+int main()
+{
+	int a[10],n;
+	printf("Enter the no. of elements in the array:");
+	scanf("%d",&n);
+	read(a,n);
+	printf("Before Sorting:\n");
+	disp(a,n);
+	sort(a,n);
+	printf("\n After Sorting:\n");
+	disp(a,n);
+	return 0;
+	
+}
